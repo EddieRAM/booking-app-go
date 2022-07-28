6799918e1d61e62f220ba7612ce67897ce6d2d0c
EddieRAM <edusacarneiro@gmail.com>
2022-07-28 11:55:29 -0300

    feat: booking slice shows only first names

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 		conferenceName := "Go Conference"
@@ -13,34 +16,39 @@ func main() {
 		fmt.Printf("Welcome to %v booking application!\n", conferenceName)
 		fmt.Printf("We have total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 		fmt.Println("Get your tickets here to attend.")
-		
-		var firstName string
-		var lastName string
-		var email string
-		var userTickets uint
-		// ask user for their name
-		// fmt.Scan(&firstName)
-		fmt.Println("Enter your first name: ")
-		fmt.Scan(&firstName)
-		
-		fmt.Println("Enter your last name: ")
-		fmt.Scan(&lastName)
-		
-		fmt.Println("Enter your email address: ")
-		fmt.Scan(&email)
-		
-		fmt.Println("How many tickets do you want?: ")
-		fmt.Scan(&userTickets)
-		
-		remainingTickets = remainingTickets - userTickets		
-		bookings = append(bookings, firstName + " " + lastName)
 
-		fmt.Printf("The whole slice: %v\n", bookings)
-		fmt.Printf("The first value: %v\n", bookings[0])
-		fmt.Printf("Slice type: %T\n", bookings)
-		fmt.Printf("Slice size: %v\n", len(bookings))
+		for {
+				var firstName string
+				var lastName string
+				var email string
+				var userTickets uint
+				
+				fmt.Println("Enter your first name: ")
+				fmt.Scan(&firstName)
+				
+				fmt.Println("Enter your last name: ")
+				fmt.Scan(&lastName)
+				
+				fmt.Println("Enter your email address: ")
+				fmt.Scan(&email)
+				
+				fmt.Println("How many tickets do you want?: ")
+				fmt.Scan(&userTickets)
+				
+				remainingTickets = remainingTickets - userTickets		
+				bookings = append(bookings, firstName + " " + lastName)
+				
+				fmt.Printf("Thank you %v %v for booking %v tickets!\nYou will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+				fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
+				// To show only the first names in the slice
 
-		fmt.Printf("Thank you %v %v for booking %v tickets!\nYou will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-		fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+				firstNames := []string{}
+				for _, booking := range bookings { // index has been replaced with _, because it has never been used 
+						var names = strings.Fields(booking)						
+						firstNames = append(firstNames, names[0]) // appends firstNames slice with firstName
+				}
+
+				fmt.Printf("The first names of the bookings list are: %v\n", firstNames)
+		}
 }
