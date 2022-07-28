91a6f2b92adb1811c26b97e7f6a5d352d2970d05
EddieRAM <edusacarneiro@gmail.com>
2022-07-28 15:29:46 -0300

    refactor: remodelled conditions for readability

--- main.go
+++ main.go
@@ -36,31 +36,31 @@ func main() {
 					fmt.Println("How many tickets do you want?: ")
 					fmt.Scan(&userTickets)
 					
-					if userTickets > remainingTickets {
-							fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
-							continue // restarts the loop
-					}
-
-					remainingTickets = remainingTickets - userTickets		
-					bookings = append(bookings, firstName + " " + lastName)
-					
-					fmt.Printf("Thank you %v %v for booking %v tickets!\nYou will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
-					fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
-					
-					// To show only the first names in the slice
-
-					firstNames := []string{}
-					for _, booking := range bookings { // index has been replaced with _, because it has never been used 
+					if userTickets <= remainingTickets {						
+						remainingTickets = remainingTickets - userTickets		
+						bookings = append(bookings, firstName + " " + lastName)
+						
+						fmt.Printf("Thank you %v %v for booking %v tickets!\nYou will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+						fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+						
+						// To show only the first names in the slice
+						
+						firstNames := []string{}
+						for _, booking := range bookings { // index has been replaced with _, because it has never been used 
 							var names = strings.Fields(booking)						
 							firstNames = append(firstNames, names[0]) // appends firstNames slice with firstName
-					}
+						}
+						
+						if remainingTickets == 0 {
+								// end program
+								fmt.Println("Our conference is booked out! Come back next year.")
+								break
+						}
 
-					fmt.Printf("The first names of bookings are: %v\n", firstNames)
-					
-					if remainingTickets == 0 {
-							// end program
-							fmt.Println("Our conference is booked out! Come back next year.")
-							break
-					}
-		}
+						fmt.Printf("The first names of bookings are: %v\n", firstNames)
+
+						} else {
+								fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)			
+						}
+			}
 }
