a47444c966e8b6bcd46b34be3365f32f46e756bf
EddieRAM <edusacarneiro@gmail.com>
2022-08-07 21:35:00 -0300

    feat: encapsulates validations and user inputs into dedicated function

--- main.go
+++ main.go
@@ -16,26 +16,10 @@ func main() {
 		fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 				
 		for {
-				var firstName string
-				var lastName string
-				var email string
-				var userTickets uint
-				
-				fmt.Println("Enter your first name: ")
-				fmt.Scan(&firstName)
-				
-					fmt.Println("Enter your last name: ")
-					fmt.Scan(&lastName)
-					
-					fmt.Println("Enter your email address: ")
-					fmt.Scan(&email)
-					
-					fmt.Println("How many tickets do you want?: ")
-					fmt.Scan(&userTickets)
+			
+					firstName, lastName, email, userTickets := getUserInputs()
 
-					isValidName := len(firstName) >= 2 && len(lastName) >=2
-					isValidEmail := strings.Contains(email, "@")
-					isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+					isValidName, isValidEmail, isValidTicketNumber := validateInputs(firstName, lastName, email, userTickets, remainingTickets)
 					
 					if isValidName && isValidEmail && isValidTicketNumber {						
 						remainingTickets = remainingTickets - userTickets		
@@ -44,7 +28,8 @@ func main() {
 						fmt.Printf("Thank you %v %v for booking %v tickets!\nYou will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 						fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 
-						pickFirstNames(bookings)
+						firstNames := pickFirstNames(bookings)
+						fmt.Printf("The first names of bookings are: %v\n", firstNames)
 						
 						// To show only the first names in the slice				
 						
@@ -75,11 +60,39 @@ func greetUsers(confName string, confTickets int, remTickets uint) {
 		fmt.Println("Get your tickets here to attend.")
 }
 
-func pickFirstNames(bookings []string) {
+func pickFirstNames(bookings []string) []string {
 	firstNames := []string{}
 	for _, booking := range bookings { // index has been replaced with _, because it has never been used 
 			var names = strings.Fields(booking)	// strings.Fields => slice manipulation method					
 			firstNames = append(firstNames, names[0]) // appends firstNames slice with firstName
 	}
-	fmt.Printf("The first names of bookings are: %v\n", firstNames)
+	return firstNames
+}
+
+func validateInputs(firstName string, lastName string, email string, userTickets uint, remainingTickets uint) (bool, bool, bool) { // when returning multiple values, the return must be encapsulated
+		isValidName := len(firstName) >= 2 && len(lastName) >=2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
+		return isValidName, isValidEmail, isValidTicketNumber // go can return multiple values
+}
+
+func getUserInputs() (string, string, string, uint){
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		
+		fmt.Println("Enter your first name: ")
+		fmt.Scan(&firstName)
+		
+		fmt.Println("Enter your last name: ")
+		fmt.Scan(&lastName)
+		
+		fmt.Println("Enter your email address: ")
+		fmt.Scan(&email)
+		
+		fmt.Println("How many tickets do you want?: ")
+		fmt.Scan(&userTickets)
+
+		return firstName, lastName, email, userTickets
 }
